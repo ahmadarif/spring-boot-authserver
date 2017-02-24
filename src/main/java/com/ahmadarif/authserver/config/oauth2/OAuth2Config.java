b064c4de45c7272a86f63972f895dfd97af31276
Ahmad Arif <ahmad.arif019@gmail.com>
2017-02-25 00:00:22 +0700

    fix config

--- src/main/java/com/ahmadarif/authserver/config/oauth2/OAuth2Config.java
+++ src/main/java/com/ahmadarif/authserver/config/oauth2/OAuth2Config.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
@@ -17,39 +16,34 @@ import javax.sql.DataSource;
  * Created by ARIF on 23-Feb-17.
  */
 @Configuration
-public class OAuth2Config {
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
-    @EnableAuthorizationServer
-    protected class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
 
-        @Autowired
-        @Qualifier("authenticationManagerBean")
-        private AuthenticationManager authenticationManager;
+    @Autowired
+    private DataSource dataSource;
 
-        @Autowired
-        private DataSource dataSource;
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
 
-        @Override
-        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.jdbc(dataSource);
-        }
-
-        @Override
-        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-            security
-                    .tokenKeyAccess("permitAll()")
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()")
 //                    .checkTokenAccess("hasRole('CLIENT')"); // field authorities harus pakai prefix (defaultnya 'ROLE_')
 //                    .checkTokenAccess("hasAuthority('CLIENT')"); // sesuai nama
-                    .checkTokenAccess("isAuthenticated()"); // rekomended
-        }
+                .checkTokenAccess("isAuthenticated()"); // rekomended
+    }
 
-        @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints
-                    .tokenStore(new InMemoryTokenStore())
-                    .authenticationManager(authenticationManager);
-        }
-
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(new InMemoryTokenStore())
+                .authenticationManager(authenticationManager);
     }
 
 }
