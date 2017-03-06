170f07eb60bc3afd74592bdb785dbe42c20ded84
Ahmad Arif <ahmad.arif019@gmail.com>
2017-03-06 21:50:28 +0700

    add global method security config

--- src/main/java/com/ahmadarif/authserver/config/SecurityConfig.java
+++ src/main/java/com/ahmadarif/authserver/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import javax.sql.DataSource;
  * Created by ARIF on 23-Feb-17.
  */
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
